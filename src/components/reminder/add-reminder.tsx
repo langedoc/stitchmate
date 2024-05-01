bbdadc37f0456b4a463c47665074eea406efb080
Daniel Ort <md.ort@gmx.de>
2024-05-01 14:59:29 +0200

    feat: cypress implemented, added one userflow-e2e-test, added data-cy property to elements used in test

--- src/components/reminder/add-reminder.tsx
+++ src/components/reminder/add-reminder.tsx
@@ -6,7 +6,7 @@ export default function AddReminder():JSX.Element {
 
   return (
     <Link className='' href="/reminders/new">
-      <div className='relative z-10 w-40 h-40 flex bg-viridian-200 rounded-xl py-3 px-4'>
+      <div data-cy="add-reminder-button" className='relative z-10 w-40 h-40 flex bg-viridian-200 rounded-xl py-3 px-4'>
         <h4 className='font-semibold text-slate-800'>Add a new reminder</h4>
         <IoAdd className='text-slate-800 self-end' size={64} />
       </div>
